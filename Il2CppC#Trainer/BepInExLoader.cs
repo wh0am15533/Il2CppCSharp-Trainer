1e4cd054baa6f8f488bd91f0f0afb867fa1b7d1b
wh0am1 <18432559+wh0am15533@users.noreply.github.com>
2020-10-10 22:49:24 -0700

    initial commit
    example on how to implement a C# mod in a Il2Cpp game and injecting a custom Type during runtime into the Il2Cpp Domain, using the preview version 6.0 of BepInEx and Doorstop

---
+++ Il2CppC#Trainer/BepInExLoader.cs
@@ -0,0 +1,113 @@
+﻿using BepInEx;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Trainer
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        #region[Declarations]
+
+        public const string
+            MODNAME = "Trainer",
+            AUTHOR = "wh0am15533",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        #endregion
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+#if DEBUG
+            log.LogMessage("[Trainer] Entered Load()");
+#endif
+
+            #region[Register TrainerComponent in Il2Cpp]
+
+            log.LogMessage("[Trainer] Registering TrainerComponent in Il2Cpp");
+
+            try
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<TrainerComponent>();
+                var go = new GameObject("TrainerObject");
+                go.AddComponent<TrainerComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("[Trainer] FAILED to Register Il2Cpp Type: TrainerComponent!");
+            }
+
+            #endregion
+
+            #region[Harmony Patching]
+
+            try
+            {
+                var harmony = new Harmony("wh0am15533.trainer.il2cpp");
+
+                /// Use USplashScreen for Testing Update() Hook (Orc's Civil War) just to make sure it works,
+                /// then look for a Update() that always exist in the game. Here are some for Orc's:
+                ///     USplashScreen loads right away, then stops after splash screen video
+                ///     ObjRotation loads when you load a level
+                ///     DigitalRubyShared.FingersScript loads when you load a level
+                ///     mGameStudio.RTS.CameraController loads when you load a level
+                ///     UnityEngine.UI.CanvasScaler loads right away and stays loaded as long as the game has a Canvas object
+                ///
+                var original = AccessTools.Method(typeof(UnityEngine.UI.CanvasScaler), "Update");
+                log.LogMessage("[Trainer] Harmony - Original Method: " + original.DeclaringType.Name + "." + original.Name);
+
+                var post = AccessTools.Method(typeof(TrainerComponent), "Update");
+                log.LogMessage("[Trainer] Harmony - Postfix Method: " + post.DeclaringType.Name + "." + post.Name);
+
+                harmony.Patch(original, postfix: new HarmonyMethod(post));
+
+                log.LogMessage("[Trainer] Harmony - Runtime Patch's Applied");
+            }
+            catch
+            {
+                log.LogError("[Trainer] Harmony - FAILED to Apply Patch's!");
+            }
+
+            #endregion
+        }
+
+    }
+}
+
+
+#region[Harmony Patches]
+/*
+public static class HarmonyPatches
+{
+    [HarmonyPostfix]
+    public static void Postfix()
+    {
+        bool isFiring = false;
+
+        //BepInExLoader.log.LogMessage("[Trainer] Entered Hooked Update()");
+
+        if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.Backspace) && !isFiring)
+        {
+            isFiring = true;
+
+            Console.WriteLine("[Trainer] TestComponent - Update() Keypress");
+
+            File.WriteAllText("C:\\Games\\Orcs Civil War\\TEST.txt", TrainerComponent.GetGameObjects());
+
+            isFiring = false;
+        }
+    }
+}
+*/
+#endregion
---
+++ Il2CppC#Trainer/TrainerComponent.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using BepInEx;
+using UnhollowerBaseLib;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using UnityEngine.SceneManagement;
+using BepInEx.IL2CPP.UnityEngine; //For UnityEngine.Input
+
+namespace Trainer
+{
+#if DEBUG
+    internal delegate void getRootSceneObjects(int handle, IntPtr list);
+#endif
+
+    public class TrainerComponent : MonoBehaviour
+    {
+        #region[Declarations]
+
+        private static GameObject levelManagerGO;
+        private static mico.game.LevelManager levelManager = null;
+
+#if DEBUG
+        private static List<GameObject> _unitButtons = new List<GameObject>();
+        private static UnitButtonManager unitButtonManager = null;
+#endif
+
+        #endregion
+
+        public TrainerComponent(IntPtr ptr) : base(ptr)
+        {
+#if DEBUG
+            BepInExLoader.log.LogMessage("[Trainer] TestComponent - Entered Constructor");
+#endif
+        }
+
+        public static void Awake()
+        {
+#if DEBUG
+            BepInExLoader.log.LogMessage("[Trainer] TestComponent - Entered Awake()");
+#endif
+        }
+
+        public static void Start()
+        {
+#if DEBUG
+            BepInExLoader.log.LogMessage("[Trainer] TestComponent - Entered Start()");
+#endif
+        }
+
+        [HarmonyPostfix]
+        public static void Update()
+        {
+#if DEBUG
+            // Dump GameObjects
+            if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.Backspace) && Event.current.type == EventType.KeyDown)
+            {
+                Event.current.Use();
+
+                string dbg = "";
+                foreach(GameObject obj in TrainerComponent.GetGameObjects())
+                {
+                    BepInExLoader.log.LogMessage("[GameObject]: " + obj.name);
+                    dbg += "[GameObject]: " + obj.name + "\r\n";
+
+                    #region[Get GameObject Components]
+
+                    BepInExLoader.log.LogMessage("\t[Components]:");
+                    dbg += "\t[Components]:\r\n";
+
+                    var comps = obj.GetGameObjectComponents();
+                    foreach(var comp in comps)
+                    {
+                        BepInExLoader.log.LogMessage("\t   " + comp.Name);
+                        dbg += "\t\t" + comp.Name + "\r\n";
+                    }
+
+                    dbg += "\r\n";
+
+                    #endregion
+
+                    #region[Get Child Objects]
+                    
+                    BepInExLoader.log.LogMessage("\t[Children]:");
+                    dbg += "\t[Children]:\r\n";
+
+                    // Get Children
+                    foreach (var child in obj.GetParentsChildren())
+                    {
+                        BepInExLoader.log.LogMessage("\t   " + child.name + ":");
+                        dbg += "\t\t" + child.name + "\r\n";
+                        
+                        BepInExLoader.log.LogMessage("\t      [Components]:");
+                        dbg += "\t\t\t[Components]:\r\n";
+                        var childcomps = child.GetGameObjectComponents();
+                        foreach(var childcomp in childcomps)
+                        {
+                            BepInExLoader.log.LogMessage("\t         " + childcomp.Name);
+                            dbg += "\t\t\t\t" + childcomp.Name + "\r\n";
+                        }
+
+                        dbg += "\r\n";
+
+                        // Get Grand Children
+                        BepInExLoader.log.LogMessage("\t      [Grand Children]:");
+                        dbg += "\t\t\t[Grand Children]:\r\n";
+
+                        foreach (var grandchild in child.GetParentsChildren())
+                        {
+                            BepInExLoader.log.LogMessage("\t         " + grandchild.name + ":");
+                            dbg += "\t\t\t\t" + grandchild.name + "\r\n";
+
+                            BepInExLoader.log.LogMessage("\t            [Components]:");
+                            dbg += "\t\t\t\t\t[Components]:\r\n";
+                            var grandchildcomps = grandchild.GetGameObjectComponents();
+                            foreach (var grandchildcomp in grandchildcomps)
+                            {
+                                BepInExLoader.log.LogMessage("\t               " + grandchildcomp.Name);
+                                dbg += "\t\t\t\t\t\t" + grandchildcomp.Name + "\r\n";
+                            }
+                        }
+                    }
+
+                    dbg += "\r\n";
+                    
+                    #endregion
+                }
+
+                File.WriteAllText("C:\\Games\\Orcs Civil War\\TEST.txt", dbg);
+            }
+#endif
+
+            #region[Orc's Cheats]
+
+            if (levelManager == null)
+            {
+                levelManagerGO = GameObject.Find("LevelManager");
+                if (levelManagerGO != null)
+                {
+                    levelManager = levelManagerGO.GetComponent<mico.game.LevelManager>();
+                    //levelManager.levelData.unitButtonVisible = true;
+                }
+            }
+            else
+            {
+                // Give Resources
+                if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F12) && Event.current.type == EventType.KeyDown)
+                {
+                    levelManager.addResources(5000);
+                    Event.current.Use();
+                }
+
+#if DEBUG
+                levelManager.levelData.startResources = 1000;
+                levelManager.levelData.enableResurrection = true;
+                levelManager.levelData.resetResurrectionTime = 100.0f;
+                levelManager.levelData.resetMeteorTime = 100.0f;
+                levelManager.levelData.meteorIsReady = true;
+                levelManager.levelData.resurrectionIsReady = true;
+#endif
+            }
+
+            // Kill All
+            if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F11) && Event.current.type == EventType.KeyDown)
+            {
+                GameObject UnitManagerGO = GameObject.Find("UnitManager");
+
+                if(UnitManagerGO != null)
+                {
+                    var comp = UnitManagerGO.GetComponent<UnitManager>();
+                    comp.killAll();
+                }
+
+                Event.current.Use();
+            }
+
+
+            #region[Enable all Unit Types (It enables the objects but they still aren't clickable)]
+            /*
+            if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F11) && Event.current.type == EventType.KeyDown)
+            {
+                GameObject unitButtonsGO = GameObject.Find("UnitButton");
+                if (unitButtonsGO != null)
+                {
+                    unitButtonManager = unitButtonsGO.GetComponent<UnitButtonManager>();
+
+                    for (int idx = 0; idx < unitButtonsGO.transform.childCount; idx++)
+                    {
+                        var child = unitButtonsGO.transform.GetChild(idx);
+                        _unitButtons.Add(child.gameObject);
+                        BepInExLoader.log.LogMessage("[GameObject]: " + child.name + " IS_ACTIVE: " + child.gameObject.activeSelf.ToString());
+                    }
+                }
+
+                Event.current.Use();
+            }
+
+            if (unitButtonManager != null)
+            {
+                foreach (var unitButton in _unitButtons)
+                {
+                    unitButtonManager.enableClick = true;
+
+                    if (unitButton.name == "Button_Orc_Shaman" || unitButton.name == "Button_Orc_Catapult")
+                    {
+                        var comp = unitButton.GetComponent<UnitButton>();
+
+                        comp.btn.enabled = true;
+                        comp.btn.interactable = true;
+                        comp.igonreCheck = true;
+                        comp.isEnabled = true;
+                        comp.show(0f);
+                        unitButton.SetActive(true);
+                    }
+                }
+            }
+            */
+            #endregion
+
+            #endregion
+        }
+
+        #region[Unity Testing]
+#if DEBUG
+        private static List<GameObject> AllLoadedGameObjects = new List<GameObject>();
+        internal static getRootSceneObjects getRootSceneObjects_iCall = IL2CPP.ResolveICall<getRootSceneObjects>("UnityEngine.SceneManagement.Scene::GetRootGameObjectsInternal");
+        public static void GetRootGameObjects_Internal(Scene scene, IntPtr list)
+        {
+            getRootSceneObjects_iCall(scene.handle, list);
+        }
+        public static Il2CppSystem.Collections.Generic.List<GameObject> TestGetRootSceneGameObjects()
+        {
+            var scene = SceneManager.GetActiveScene();
+            var list = new Il2CppSystem.Collections.Generic.List<GameObject>(scene.rootCount);
+
+            GetRootGameObjects_Internal(scene, list.Pointer);
+
+            return list;
+        }
+
+        public static List<GameObject> GetGameObjects()
+        {
+            foreach (var gobject in TestGetRootSceneGameObjects())
+            {
+                AllLoadedGameObjects.Add(gobject);
+            }
+            return AllLoadedGameObjects;
+        }
+#endif
+    #endregion
+
+    }
+
+    #region[GameObjects Extensions]
+#if DEBUG
+    public static class GameObjectExtensions
+    {
+        public static bool HasComponent<T>(this GameObject flag) where T : Component
+        {
+            if (flag == null)
+                return false;
+            return flag.GetComponent<T>() != null;
+        }
+
+        public static List<GameObject> GetParentsChildren(this GameObject parent)
+        {
+            if (parent == null)
+                return null;
+            List<GameObject> tmp = new List<GameObject>();
+
+            for (int idx = 0; idx < parent.transform.childCount; idx++)
+            {
+                tmp.Add(parent.transform.GetChild(idx).gameObject);
+            }
+
+            return tmp;
+        }
+
+        public static List<Il2CppSystem.Type> GetGameObjectComponents(this GameObject gameObject)
+        {
+            if (gameObject == null)
+                return null;
+            List<Il2CppSystem.Type> tmp = new List<Il2CppSystem.Type>();
+
+            var comps = gameObject.GetComponents<Component>();
+            foreach (var comp in comps)
+            {
+                tmp.Add(comp.GetIl2CppType());
+            }
+
+            return tmp;
+        }
+    }
+#endif
+    #endregion
+}
+
+/*
+            if (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.F11) && Event.current.type == EventType.KeyDown)
+            {
+                GameObject unitButtonsGO = GameObject.Find("UnitButton");
+                if (unitButtonsGO != null)
+                {
+                    unitButtonManager = unitButtonsGO.GetComponent<UnitButtonManager>();
+
+                    for (int idx = 0; idx < unitButtonsGO.transform.childCount; idx++)
+                    {
+                        var child = unitButtonsGO.transform.GetChild(idx);
+
+                        if (child.name == "Button_Orc_Shaman")
+                        {
+                            var comp = child.gameObject.GetComponent<UnitButton>();
+
+                            comp.btn.enabled = true;
+                            comp.btn.interactable = true;
+                            //comp.igonreCheck = true;
+                            comp.isEnabled = true;
+                            comp.show(0f);
+                            child.gameObject.SetActive(true);
+
+                            //unitButtonManager._currentUnitIndex = 6;
+                            //unitButtonManager.activeBtnByIndex(6);
+                            unitButtonManager.activeButton(comp, true);
+                            unitButtonManager._isReadyToBuildUnit = true;
+                            unitButtonManager.enableClick = true;
+                        }
+
+                        //BepInExLoader.log.LogMessage("[GameObject]: " + child.name + " IS_ACTIVE: " + child.gameObject.activeSelf.ToString());
+                    }
+                }
+
+                Event.current.Use();
+            }
+
+*/
